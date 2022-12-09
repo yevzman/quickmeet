b46d6aa25bc37bd10d6d00420b70cda33bb1a817
Evzman Yan <yalevzman@edu.hse.ru>
2022-12-09 03:51:20 +0300

    project MVP

--- src/api/solver.py
+++ src/api/solver.py
@@ -1,4 +1,4 @@
-from data import Data
+from .data import Data
 
 from amadeus import Client
 
@@ -15,7 +15,7 @@ class Solver:
             client_secret=str(self.__api_secret)
         )
     
-    def get_flight_offers(self, orig, dest, date):
+    def _get_flight_offers(self, orig, dest, date):
         response = self.amadeus.shopping.flight_offers_search.get(
             originLocationCode=orig,
             destinationLocationCode=dest,
@@ -24,40 +24,51 @@ class Solver:
         )
         return response.data
     
-    def get_cheapest_price(self, orig, dest, date):
-        data = self.get_flight_offers(orig, dest, date)
+    def _get_cheapest_price(self, orig, dest, date):
+        data = self._get_flight_offers(orig, dest, date)
         now = 1000000000.0
         for item in data:
             price = float(item['price']['total'])
             now = min(now, price)
+        if now == 1000000000.0:
+            raise "Bad flight"
         return now
 
-    def get_cheapest_meeting(self, people, date):
-        d = Data('data.txt')
+    def get_cheapest_meeting(self, people: list, date):
+        d = Data('./api/data.txt')
 #        cities = d.get_cities()
         cities = ['Moscow', 'St Petersburg', 'Novosibirsk', 'Ekaterinburg',
                     'Kazan', 'Nizhniy Novgorod', 'Chelyabinsk', 'Krasnojarsk',
                     'Samara', 'Ufa', 'Omsk', 'Krasnodar', 'Voronezh', 'Perm', 'Volgograd']
         result = []
+        suitable_city = True
         for city in cities[:2]:
             sum = 0.0
+            print('City:', city)
             for person in people:
                 if person.city == city:
                     continue
                 airports1 = d.get_airports(person.city)
                 airports2 = d.get_airports(city)
                 min_price = 1000000000.0
+                any_flights = False
                 for air1 in airports1:
                     for air2 in airports2:
                         try:
                             print(air1[0], air2[0])
-                            price = self.get_cheapest_price(air1[0], air2[0], date)
+                            price = self._get_cheapest_price(air1[0], air2[0], date)
+                            any_flights = True
+                            print('flight', air1[0], air2[0], date, 'price', price)
                             min_price = min(min_price, price)
                         except:
                             continue
-                sum += min_price
-            result.append([sum, city])
+                if any_flights:
+                    sum += min_price
+                else:
+                    suitable_city = False
+            if suitable_city:
+                result.append([sum, city])
         result.sort()
         if len(result) > 0:
-            return result[0][1]
+            return result[0][0], result[0][1]
         return None
--- src/main.py
+++ src/main.py
@@ -1,8 +1,10 @@
 """Телеграм бот, который ..."""
 import os
+import sys
 import logging
 import telebot
-from db.bot_db import create_table, BotDB
+from db.bot_db import BotDB, DBStatus
+from api.solver import Person, Solver
 
 token = os.environ['TELEGRAM_BOT_TOKEN']
 bot = telebot.TeleBot(token)
@@ -11,6 +13,9 @@ FORMAT = '%(asctime)s - [%(levelname)s] -  %(name)s - (%(filename)s).%(funcName)
 logging.basicConfig(format=FORMAT)
 logger = logging.getLogger(__name__)
 
+# auxiliary tools
+table: BotDB = None
+solver: Solver = Solver()
 
 @bot.message_handler(commands=['start'], content_types=['text'])
 def start_handler(message):
@@ -23,12 +28,13 @@ def help_handler(message):
     text = 'Привет! Список допустимых команд: \n' \
            '/help - короткая информация о возможностях бота\n' \
            '/create_group groupName - создать группу с названием groupName и возвращает groupId, ' \
-                                           'по которому в дальнейшем можно делать преобразования с ней\n' \
-           '/add_user groupId userName City\n добавить участника в группу groupId, с именем userName, ' \
+                                           'по которому в дальнейшем можно делать операции с ней\n' \
+           '/add_user groupId groupName userName City\n добавить участника в группу groupId, с именем userName, ' \
                                                                 'который будет лететь из города City\n' \
-           '/delete_user groupId userName City - удалить пользователя с именем userName из группы groupId\n' \
-           '/view_group groupId - посмотреть список всех участников группы groupId\n' \
-           '/find_flights groupId variantsAmount - найти город, в который дешевле всего будет попасть группе groupId'
+           '/delete_user groupId groupName userName City - удалить пользователя с именем userName из группы groupId\n' \
+           '/view_group groupId groupName - посмотреть список всех участников группы groupId\n' \
+           '/find_flights groupId groupName date - найти город, в который дешевле всего будет попасть группе groupId.\n' \
+           'Формат даты: YYYY-MM-DD'
     bot.send_message(message.from_user.id, text)
 
 
@@ -41,10 +47,13 @@ def create_group_handler(message):
         bot.send_message(message.from_user.id, text)
         return
     group_name = args[1]
-    group_id = table.add_group(group_name)
-
-    text = f'Созданна группа с названием {group_name}\n' \
-           f'groupId - {group_id}'
+    group_id, status = table.add_group(group_name)
+    logger.info(f'Process of creating group with name {group_name} finished with status: {status}')
+    if status == DBStatus.SUCCESS:
+        text = f'Созданна группа с названием {group_name}\n' \
+               f'groupId - {group_id}'
+    else:
+        text = 'Возникли проблемы!'
 
     bot.send_message(message.from_user.id, text)
 
@@ -52,59 +61,90 @@ def create_group_handler(message):
 @bot.message_handler(commands=['add_user'], content_types=['text'])
 def add_user_handler(message):
     args = message.text.split()
-    if len(args) < 4:
+    if len(args) < 5:
         logger.warning('Too few arguments')
         text = 'Недостаточно аргументов для добавления пользователя. Попробуйте еще раз!'
         bot.send_message(message.from_user.id, text)
         return
     group_id = args[1]
-    user_name = args[2]
-    city = args[3]
-    # table.add_user(group_id, group_name, user_name, city)
-    text = f'Добавлен пользователь в группу с id {group_id}'
+    group_name = args[2]
+    user_name = args[3]
+    city = args[4]
+
+    status = table.add_user(group_id, group_name, user_name, city)
+    logger.info(f'add user {user_name} status: {status}')
+    if status == DBStatus.SUCCESS:
+        text = f'Добавлен пользователь в группу с id {group_id}'
+    else:
+        text = 'Возникли проблемы!'
     bot.send_message(message.from_user.id, text)
 
 
 @bot.message_handler(commands=['delete_user'], content_types=['text'])
 def delete_user_handler(message):
     args = message.text.split()
-    if len(args) < 3:
+    if len(args) < 4:
         logger.warning('Too few arguments')
         text = 'Недостаточно аргументов для удаления пользователя. Попробуйте еще раз!'
         bot.send_message(message.from_user.id, text)
         return
     group_id = args[1]
-    user_name = args[2]
-    # table.delete_user(group_id, 'group_name?')
-    text = f'Удален пользователь из группы с id {group_id}'
+    group_name = args[2]
+    user_name = args[3]
+
+    status = table.delete_user(group_id, group_name, user_name)
+    logger.info(f'delete user {user_name} status: {status}')
+    if status == DBStatus.SUCCESS:
+        text = f'Удален пользователь из группы с id {group_id}'
+    else:
+        text = 'Возникли проблемы!'
     bot.send_message(message.from_user.id, text)
 
 
 @bot.message_handler(commands=['view_group'], content_types=['text'])
 def view_group_handler(message):
     args = message.text.split()
-    if len(args) < 2:
+    if len(args) < 3:
         logger.warning('Too few arguments')
         text = 'Недостаточно аргументов для просмотра группы. Попробуйте еще раз!'
         bot.send_message(message.from_user.id, text)
         return
     group_id = args[1]
-    # print('View', table.view_group(group_id, group_name))
-    text = f'Список участников группы с id {group_id}:'
+    group_name = args[2]
+
+    group_list = table.view_group(group_id, group_name)
+    logger.info(f'View group {group_name} info: {group_list}')
+    text = f'Список участников группы с id {group_id}:\n'
+    if group_list is not None:
+        for user in  group_list:
+            text += f'[name={user[0]}, city={user[1]}]\n'
     bot.send_message(message.from_user.id, text)
 
 
 @bot.message_handler(commands=['find_flights'], content_types=['text'])
 def find_flights_handler(message):
     args = message.text.split()
-    if len(args) < 2:
+    if len(args) < 4:
         logger.warning('Too few arguments')
         text = 'Недостаточно аргументов для просмотра группы. Попробуйте еще раз!'
         bot.send_message(message.from_user.id, text)
         return
     group_id = args[1]
-    flights_amount = 1 if len(args) < 3 else int(args[2])
-    text = 'Пока что не умею искать билеты...'
+    group_name = args[2]
+    date = args[3]
+
+    group_list = table.view_group(group_id, group_name)
+    people = []
+    for user in group_list:
+        people.append(Person(user[1]))
+    info = solver.get_cheapest_meeting(people, date)
+    if info is not None:
+        text = f'Flight info:\n' \
+               f'City: {info[1]}\n' \
+               f'Price: {info[0]}'
+    else:
+        text = 'Нет информации'
+    logger.info(f'Flight info: {info}')
     bot.send_message(message.from_user.id, text)
 
 
@@ -119,6 +159,7 @@ if __name__ == '__main__':
     logger.setLevel(level='INFO')
     logger.info('Starting bot')
     db_path = 'db/data.db'  # Позже будет доставаться из окружения
-    logger.info('create_table status: ' + str(create_table(db_path)))
     table = BotDB(db_path)
+    if len(sys.argv) == 1 or sys.argv[1] == '1':
+        logger.info(f'create_table status: {table.recreate_table()}')
     bot.polling(none_stop=True, interval=0)
